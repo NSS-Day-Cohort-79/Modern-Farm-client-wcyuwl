3cbcd388aa8f5a3d5de32275fbebc1837aad7ff9
Maggie Flatt <maggieflatt19@gmail.com>
2025-11-21 09:22:58 -0600

    added seeds and test code to main

--- src/scripts/seeds/corn.js
+++ src/scripts/seeds/corn.js
@@ -1,1 +1,15 @@
 //export const createCorn = () => {} return an array with 2 identical objects
+export const createCorn = () => {
+    return [
+        {
+        type: "Corn",
+        height: 180,
+        output: 6
+        },
+        {
+        type: "Corn",
+        height: 180,
+        output: 6
+        }
+    ]
+}
---
+++ src/scripts/seeds/soybean.js
@@ -0,0 +1,7 @@
+export const createSoybean = () => {
+    return {
+        type: "Soybean",
+        height: 2,
+        output: 4
+    }
+}
