a49fbc426338d79543ff88ed84bcc75d82e76091
Jack Gardner <jtgardner99@gmail.com>
2025-11-21 14:02:09 -0600

    catalog complete

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,6 +1,6 @@
 //import { createPlan } from ./main.js
 import { Catalog } from "./catalog.js"
-import { addPlant, usePlants } from "./field.js"
+import { usePlants } from "./field.js"
 import { harvestPlants } from "./harvester.js"
 import { plantSeeds } from "./tractor.js"
 import { createPlan } from "./plan.js"
@@ -18,6 +18,7 @@ console.log(usedPlants)
 //invoke harvestPlants 
 const harvestedPlants = harvestPlants(usedPlants)
 console.log(harvestedPlants)
+
 Catalog(harvestedPlants)
 //console.log(catalogHTML)
 //document.querySelector(".container").innerHTML = catalogHTML
